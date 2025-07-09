0c951af806c50554095e871f2b115305867814b9
Nathaniel Tampus <nate@mugnavo.com>
2025-07-10 06:02:46 +0800

    use custom vite react plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
@@ -10,28 +11,29 @@ export default defineConfig({
     }),
     tailwindcss(),
     tanstackStart({
-      // https://react.dev/learn/react-compiler
-      react: {
-        babel: {
-          plugins: [
-            [
-              "babel-plugin-react-compiler",
-              {
-                target: "19",
-              },
-            ],
-          ],
-        },
-      },
+      // https://github.com/TanStack/router/discussions/2863#discussioncomment-13713677
+      customViteReactPlugin: true,
 
       tsr: {
         quoteStyle: "double",
         semicolons: true,
-        // verboseFileRoutes: false,
       },
 
       // https://tanstack.com/start/latest/docs/framework/react/hosting#deployment
       // target: "node-server",
+    }),
+    react({
+      // https://react.dev/learn/react-compiler
+      babel: {
+        plugins: [
+          [
+            "babel-plugin-react-compiler",
+            {
+              target: "19",
+            },
+          ],
+        ],
+      },
     }),
   ],
 });
