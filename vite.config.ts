e583f04dc9ec36657acaab66f77ec8c55d2452fd
Nathaniel Tampus <nate@mugnavo.com>
2025-08-23 16:03:58 +0800

    add @tanstack/devtools-vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import tailwindcss from "@tailwindcss/vite";
+import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -6,6 +7,7 @@ import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
+    devtools(),
     tsConfigPaths({
       projects: ["./tsconfig.json"],
     }),
