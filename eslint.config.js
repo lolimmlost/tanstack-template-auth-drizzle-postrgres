09d73510abe5d37ae10dfb9e2cf9cbea2f350beb
Nathaniel Tampus <nate@mugnavo.com>
2025-08-30 13:32:20 +0800

    update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,8 +3,10 @@ import js from "@eslint/js";
 import pluginQuery from "@tanstack/eslint-plugin-query";
 import pluginRouter from "@tanstack/eslint-plugin-router";
 import eslintConfigPrettier from "eslint-config-prettier";
-import * as reactHooks from "eslint-plugin-react-hooks";
+import reactHooks from "eslint-plugin-react-hooks";
 import tseslint from "typescript-eslint";
+
+const { plugins, ...reactHooksConfig } = reactHooks.configs.recommended;
 
 export default tseslint.config({
   ignores: ["dist", ".wrangler", ".vercel", ".netlify", ".output", "build/"],
@@ -16,19 +18,21 @@ export default tseslint.config({
       tsconfigRootDir: import.meta.dirname,
     },
   },
+  plugins: {
+    "react-hooks": reactHooks,
+  },
   extends: [
     js.configs.recommended,
     ...tseslint.configs.recommended,
     eslintConfigPrettier,
     ...pluginQuery.configs["flat/recommended"],
     ...pluginRouter.configs["flat/recommended"],
-    reactHooks.configs.recommended,
+    reactHooksConfig,
     react.configs["recommended-type-checked"],
     // ...you can add plugins or configs here
   ],
   rules: {
     // You can override any rules here
-    "react-hooks/react-compiler": "warn",
     "@typescript-eslint/no-deprecated": "warn",
   },
 });
